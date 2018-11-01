19c1964f49d926491ebb24db9b6e4b409cb660bf
zetkno <jiuminghan@163.com>
2018-11-01 20:06:16 +0800

    cj

--- bootStart/src/main/java/org/zetkno/bootStart/controller/UserController.java
+++ bootStart/src/main/java/org/zetkno/bootStart/controller/UserController.java
@@ -8,48 +8,62 @@ import org.zetkno.bootStart.pojo.User;
 import java.util.*;
 
 @RestController
-@RequestMapping(value="/users")  //使下面的映射都在/users下
+//@RequestMapping(value="/users")  //使下面的映射都在/users下
 public class UserController {
+
+    @RequestMapping("/user")
+    public String index() {
+        return "user";
+    }
 
     @Autowired
     UserMapper userMapper;
 
-    //http://localhost:8888/getUser?username=xiaoli2
-    @RequestMapping("/getUser")
-    public String getUser(String username){
-        User user =userMapper.findUserByUsername(username);
-        return user!=null ? username+"的密码是："+user.getUser_password():"不存在用户名为"+username+"的用户";
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public List<User> getUserList() {
+        // 处理"/users/"的GET请求，用来获取用户列表
+        // 还可以通过@RequestParam从页面中传递参数来进行查询条件或者翻页信息的传递
+        List<User> r = new ArrayList<User>(users.values());
+        return r;
     }
-
-    //http://localhost:8888/updateUser?username=xiaoli2&password=123
-    @RequestMapping("/updateUser")
-    public String updateUser(String password,String username){
-        User user = new User(username,password);
-        userMapper.updateUserByUsername(user);
-        return "success!";
-    }
-
-
-    //http://localhost:8888/addUser?username=xiaoli2&password=123
-    @RequestMapping("/addUser")
-    public String addUser(String username,String password){
-        User user = new User(username,password);
-        userMapper.saveUser(user);
-        return "success!";
-    }
-
-    //http://localhost:8888/addUser?username=xiaoli2
-    @RequestMapping("/deleteUser")
-    public String deleteUser(String username){
-        userMapper.deleteUserByUsername(username);
-        return "success!";
-    }
-
-    //http://localhost:8888/getUserList
-    @RequestMapping("/getUserList")
-    public List getUserList(String username, String password){
-        return userMapper.getUserList();
-    }
+    public
+//
+//    //http://localhost:8888/getUser?username=xiaoli2
+//    @RequestMapping("/getUser")
+//    public String getUser(String username){
+//        User user =userMapper.findUserByUsername(username);
+//        return user!=null ? username+"的密码是："+user.getUser_password():"不存在用户名为"+username+"的用户";
+//    }
+//
+//    //http://localhost:8888/updateUser?username=xiaoli2&password=123
+//    @RequestMapping("/updateUser")
+//    public String updateUser(String password,String username){
+//        User user = new User(username,password);
+//        userMapper.updateUserByUsername(user);
+//        return "success!";
+//    }
+//
+//
+//    //http://localhost:8888/addUser?username=xiaoli2&password=123
+//    @RequestMapping("/addUser")
+//    public String addUser(String username,String password){
+//        User user = new User(username,password);
+//        userMapper.saveUser(user);
+//        return "success!";
+//    }
+//
+//    //http://localhost:8888/addUser?username=xiaoli2
+//    @RequestMapping("/deleteUser")
+//    public String deleteUser(String username){
+//        userMapper.deleteUserByUsername(username);
+//        return "success!";
+//    }
+//
+//    //http://localhost:8888/getUserList
+//    @RequestMapping("/getUserList")
+//    public List getUserList(String username, String password){
+//        return userMapper.getUserList();
+//    }
 
 
 
